c5d3610c97068aad7facd4f745dce820d90c7f86
Teuku Aidil Nur <teukuaidilnur@gmail.com>
2026-01-22 15:58:59 +0700

    Belajar JS

---
+++ coba.js
@@ -0,0 +1,32 @@
+const namaku = "Teuku Aidil Nur";
+console.log(namaku);
+
+// peraturan variabel
+
+const namaKu = teukuaidilnur;
+console.log(namaKu);
+
+const namadepan = "Teuku";
+const namabelakang = "Aidil";
+const namalengkap = "Teuku Aidil";
+
+console.log(namadepan);
+console.log(namabelakang);
+console.log(namalengkap);
+
+//number
+
+const a = 10;
+const b = 20;
+
+const penjumlahan = a + b;
+console.log(penjumlahan);
+
+const pengurangan = b - a;
+console.log(pengurangan);
+
+const perkalian = a * b;
+console.log(perkalian);
+
+const pembagian = b / a;
+console.log(pembagian);
