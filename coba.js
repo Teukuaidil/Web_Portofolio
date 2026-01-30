bb2ccab12a6fb9009af5d26cca23a514dad262ae
Teuku Aidil Nur <teukuaidilnur@gmail.com>
2026-01-30 23:09:29 +0700

    Belajar JS_1

--- coba.js
+++ coba.js
@@ -1,32 +1,35 @@
-const namaku = "Teuku Aidil Nur";
-console.log(namaku);
+// let angka = Math.random();
+// console.log(angka);
 
-// peraturan variabel
+// if (angka > 0.5) {
+//   console.log("Angka lebih besar dari 0.5");
+// }
 
-const namaKu = teukuaidilnur;
-console.log(namaKu);
+// if (angka < 0.5) {
+//   console.log("Angka lebih kecil dari 0.5");
+// }
 
-const namadepan = "Teuku";
-const namabelakang = "Aidil";
-const namalengkap = "Teuku Aidil";
+const inihari = "Rabu";
 
-console.log(namadepan);
-console.log(namabelakang);
-console.log(namalengkap);
+if (inihari === "senin") {
+  console.log("Selamat bekerja!");
+} else if (inihari === "sabtu") {
+  console.log("Selamat berakhir pekan!");
+} else if (inihari === "minggu") {
+  console.log("Selamat beristirahat!");
+} else {
+  console.log("Hari biasa saja.");
+}
 
-//number
-
-const a = 10;
-const b = 20;
-
-const penjumlahan = a + b;
-console.log(penjumlahan);
-
-const pengurangan = b - a;
-console.log(pengurangan);
-
-const perkalian = a * b;
-console.log(perkalian);
-
-const pembagian = b / a;
-console.log(pembagian);
+const angka = 50;
+if (angka > 80) {
+  console.log("A");
+} else if (angka > 70) {
+  console.log("B");
+} else if (angka > 60) {
+  console.log("C");
+} else if (angka > 50) {
+  console.log("D");
+} else {
+  console.log("E");
+}
